b5561842c7a0d4ad62591b9d65a6424c4f5a8a32
Inari Aaltojärvi <ext.inari.aaltojarvi@cargotec.com>
2020-05-20 10:00:54 +0300

    Added logger, middleware and config under utils. Controller to route the app. Delete and put work.

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
-require('dotenv').config()
+const config = require('./utils/config')
 const http = require('http')
 const express = require('express')
-const cors = require('cors')
 const app = express()
-const Blog = require('./models/blog')
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 
+app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-app.use(cors())
+app.use(middleware.requestLogger)
 
+app.use('/api/blogs', blogsRouter)
 
-app.get('/', (req, res) => {
-    res.send('<h1>Blog</h1>')
-  })
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT
+const PORT = config.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- models/blog.js
+++ models/blog.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose')
-require('dotenv').config()
+const config = require('../utils/config')
+const logger = require('../utils/logger')
 
-const url = process.env.MONGODB_URI;
+const url = config.MONGODB_URI;
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => {
-    console.log('connected to MongoDB')
+    logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
+    logger.error('error connecting to MongoDB:', error.message)
   })
 
 
 const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
+    title: {type: String, required: true, minlength: 2},
+    author: {type: String, required: true, minlength: 2},
+    url:  {type: String, required: true, minlength: 10},
     likes: Number
   })
 
